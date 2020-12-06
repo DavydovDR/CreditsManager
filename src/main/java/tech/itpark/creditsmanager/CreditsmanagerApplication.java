d9140b0afad12e4fc17ca37ec44432050e37856b
DavydovDR <mymail06.88@mail.ru>
2020-12-06 20:04:36 +0300

    init project

---
+++ src/main/java/tech/itpark/creditsmanager/CreditsmanagerApplication.java
@@ -0,0 +1,13 @@
+package tech.itpark.creditsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditsmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditsmanagerApplication.class, args);
+    }
+
+}
