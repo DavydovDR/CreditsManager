0a0bf02c25bab4220260e3db6b3bf49221b0d398
student <student@itpark.tech>
2020-12-20 09:52:48 +0300

    fix bags in year percents

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO credits (id, name, sum, createddate, payday, percent, months) VALUES
-(1, 'ipoteka', 240000000, '2018-01-01', 28, 920, 180),
-(2, 'avto', 100000000, '2020-10-10', 20, 1420, 120);
+(1, 'ipoteka', 240000000, '2018-01-01', 28, 92, 180),
+(2, 'avto', 100000000, '2020-10-10', 20, 142, 120);
 
 INSERT INTO payments (id, paySum, payDate, mainDebt, creditId) VALUES
 (1, 500000, '2020-01-01', 250000, 2),
